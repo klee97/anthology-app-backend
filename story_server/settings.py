7c831f82184544ec8c64da95b4942714da79558d
klee97 <klee9700@gmail.com>
2025-01-30 09:51:56 -0800

    Add database url for disco

--- story_server/settings.py
+++ story_server/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-# import os
-# import dj_database_url
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'story_server.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': #dj_database_url.config(default=os.getenv('DATABASE_URL')),
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
     #'local': 
-    {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stories_db',
-        'USER': 'stories_user',
-        'PASSWORD': 'gTujst6og4Ch!sdt8S',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    # {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'stories_db',
+    #     'USER': 'stories_user',
+    #     'PASSWORD': 'gTujst6og4Ch!sdt8S',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     
 }
 
