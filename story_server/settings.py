4797d1e4851f79c117e2352264df0c8757aa90f6
klee97 <klee9700@gmail.com>
2025-01-30 12:17:45 -0800

    Add csrf trusted origins

--- story_server/settings.py
+++ story_server/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["anthology.rcdis.co", "localhost", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS=['https://*.anthology.rcdis.co', 'https://*.127.0.0.1']
 
 # Application definition
 
