fbd7fb0cfff9b286c2c39ab956eef51f6c5edf08
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2025-04-08 05:11:16 -0400

    add django rest framework

--- story_server/settings.py
+++ story_server/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "stories"
+    'rest_framework',
+    'stories'
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly']
+}
